8e0f344677570a7ddef8526018b52262268d3fc6
Yufen Chen <2502089c@student.gla.ac.uk>
2020-02-09 13:40:28 +0000

    09022020ch3exercise

--- rango/views.py
+++ rango/views.py
@@ -13,3 +13,15 @@ def index(request):
     #we make use of the shortcut function to make our lives easier
     #note that the first parameter is the template we wish to use
     return render(request,'rango/index.html',context=context_dict)
+
+
+def about(request):
+    #construct a dictionary to pass to the template engine as its conext
+    #note the key boldmessage matches to {{boldmessage}} in the template
+    context_dict = {'boldmessage':'Rango says here is the about page.'}
+    
+    
+    #return a rendered response to send to the client
+    #we make use of the shortcut function to make our lives easier
+    #note that the first parameter is the template we wish to use
+    return render(request,'rango/about.html',context=context_dict)
