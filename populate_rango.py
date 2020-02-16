91e5b0b59ed622ad4206a4231f6ef1d238ebe348
Yufen Chen <2502089c@student.gla.ac.uk>
2020-02-16 02:26:25 +0000

    16022020ch6exercise

--- populate_rango.py
+++ populate_rango.py
@@ -13,32 +13,32 @@ def populate():
  # through each data structure, and add the data to our models.
 
     python_pages = [
-    {'title': 'Official Python Tutorial', 'url':'http://docs.python.org/3/tutorial/'},
-    {'title':'How to Think like a Computer Scientist', 'url':'http://www.greenteapress.com/thinkpython/'},
-    {'title':'Learn Python in 10 Minutes', 'url':'http://www.korokithakis.net/tutorials/python/'} ]
+    {'title': 'Official Python Tutorial', 'url':'http://docs.python.org/3/tutorial/', 'views':2},
+    {'title':'How to Think like a Computer Scientist', 'url':'http://www.greenteapress.com/thinkpython/', 'views':2},
+    {'title':'Learn Python in 10 Minutes', 'url':'http://www.korokithakis.net/tutorials/python/', 'views':2} ]
 
     django_pages = [
-    {'title':'Official Django Tutorial', 'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
-    {'title':'Django Rocks', 'url':'http://www.djangorocks.com/'},
-    {'title':'How to Tango with Django', 'url':'http://www.tangowithdjango.com/'} ]
+    {'title':'Official Django Tutorial', 'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/', 'views':2},
+    {'title':'Django Rocks', 'url':'http://www.djangorocks.com/', 'views':2},
+    {'title':'How to Tango with Django', 'url':'http://www.tangowithdjango.com/', 'views':2} ]
 
     other_pages = [
-    {'title':'Bottle', 'url':'http://bottlepy.org/docs/dev/'},
-    {'title':'Flask', 'url':'http://flask.pocoo.org'} ]
+    {'title':'Bottle', 'url':'http://bottlepy.org/docs/dev/', 'views':2},
+    {'title':'Flask', 'url':'http://flask.pocoo.org', 'views':2} ]
 
     cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64}, 'Django': {'pages': django_pages, 'views': 64, 'likes': 32}, 'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16}}
 
-# If you want to add more categories or pages,
-# add them to the dictionaries above.
+    # If you want to add more categories or pages,
+    # add them to the dictionaries above.
 
-# The code below goes through the cats dictionary, then adds each category,
-# and then adds all the associated pages for that category.
+    # The code below goes through the cats dictionary, then adds each category,
+    # and then adds all the associated pages for that category.
     for cat, cat_data in cats.items():
         c = add_cat(cat, cat_data['views'], cat_data['likes'])
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            add_page(c, p['title'], p['url'], p['views'])
 
-# Print out the categories we have added.
+    # Print out the categories we have added.
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
             print(f'- {c}: {p}')
--- rango/admin.py
+++ rango/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
 from rango.models import Category, Page
 
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
+    
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}   
  
-admin.site.register(Category)
+
 admin.site.register(Page, PageAdmin)
+admin.site.register(Category, CategoryAdmin)
