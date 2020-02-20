ecda7b0936c7ad037674a7daad3851d98fc774d0
Yufen Chen <2502089c@student.gla.ac.uk>
2020-02-20 11:32:49 +0000

    20022020ch9ex

--- populate_rango.py
+++ populate_rango.py
@@ -1,62 +1,145 @@
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
-
 import django
 django.setup()
 from rango.models import Category, Page
 
 def populate():
- # First, we will create lists of dictionaries containing the pages
- # we want to add into each category.
- # Then we will create a dictionary of dictionaries for our categories.
- # This might seem a little bit confusing, but it allows us to iterate
- # through each data structure, and add the data to our models.
 
     python_pages = [
-    {'title': 'Official Python Tutorial', 'url':'http://docs.python.org/3/tutorial/', 'views':2},
-    {'title':'How to Think like a Computer Scientist', 'url':'http://www.greenteapress.com/thinkpython/', 'views':2},
-    {'title':'Learn Python in 10 Minutes', 'url':'http://www.korokithakis.net/tutorials/python/', 'views':2} ]
+
+        {'title': 'Official Python Tutorial',
+
+         'url':'http://docs.python.org/3/tutorial/',
+
+         'views': 114,},
+
+        {'title':'How to Think like a Computer Scientist',
+
+         'url':'http://www.greenteapress.com/thinkpython/',
+
+         'views': 53},
+
+        {'title':'Learn Python in 10 Minutes',
+
+         'url':'http://www.korokithakis.net/tutorials/python/',
+
+         'views': 20} ]
+
+    
 
     django_pages = [
-    {'title':'Official Django Tutorial', 'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/', 'views':2},
-    {'title':'Django Rocks', 'url':'http://www.djangorocks.com/', 'views':2},
-    {'title':'How to Tango with Django', 'url':'http://www.tangowithdjango.com/', 'views':2} ]
+
+        {'title':'Official Django Tutorial',
+
+         'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/',
+
+         'views': 32},
+
+        {'title':'Django Rocks',
+
+         'url':'http://www.djangorocks.com/',
+
+         'views': 12},
+
+        {'title':'How to Tango with Django',
+
+         'url':'http://www.tangowithdjango.com/',
+
+         'views': 1258} ]
+
+    
 
     other_pages = [
-    {'title':'Bottle', 'url':'http://bottlepy.org/docs/dev/', 'views':2},
-    {'title':'Flask', 'url':'http://flask.pocoo.org', 'views':2} ]
 
-    cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64}, 'Django': {'pages': django_pages, 'views': 64, 'likes': 32}, 'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16}}
+        {'title':'Bottle',
 
-    # If you want to add more categories or pages,
-    # add them to the dictionaries above.
+         'url':'http://bottlepy.org/docs/dev/',
 
-    # The code below goes through the cats dictionary, then adds each category,
-    # and then adds all the associated pages for that category.
+         'views': 54},
+
+        {'title':'Flask',
+
+         'url':'http://flask.pocoo.org',
+
+         'views': 64} ]
+
+    
+
+    no_pages = []
+
+    
+
+    cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
+
+            'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
+
+            'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16},
+
+            'Pascal': {'pages': no_pages, 'views': 0, 'likes': 0},
+
+            'Perl': {'pages': no_pages, 'views': 0, 'likes': 0},
+
+            'PHP': {'pages': no_pages, 'views': 0, 'likes': 0},
+
+            'Prolog': {'pages': no_pages, 'views': 0, 'likes': 0},
+
+            'PostScript': {'pages': no_pages, 'views': 0, 'likes': 0},
+
+            'Programming': {'pages': no_pages, 'views': 0, 'likes': 0},}
+
+    
+
     for cat, cat_data in cats.items():
-        c = add_cat(cat, cat_data['views'], cat_data['likes'])
-        for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'], p['views'])
 
-    # Print out the categories we have added.
+        c = add_cat(cat, views=cat_data['views'], likes=cat_data['likes'])
+
+        for p in cat_data['pages']:
+
+            add_page(c, p['title'], p['url'], views=p['views'])
+
+    
+
     for c in Category.objects.all():
+
         for p in Page.objects.filter(category=c):
+
             print(f'- {c}: {p}')
 
+
+
 def add_page(cat, title, url, views=0):
+
     p = Page.objects.get_or_create(category=cat, title=title)[0]
+
     p.url=url
+
     p.views=views
+
     p.save()
+
     return p
 
+
+
 def add_cat(name, views=0, likes=0):
-    c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
+
+    c = Category.objects.get_or_create(name=name)[0]
+
+    c.views = views
+
+    c.likes = likes
+
     c.save()
+
     return c
 
 
+
 # Start execution here!
+
 if __name__ == '__main__':
+
     print('Starting Rango population script...')
+
     populate()
--- rango/views.py
+++ rango/views.py
@@ -6,6 +6,10 @@ from rango.forms import CategoryForm, PageForm
 from django.shortcuts import redirect
 from django.shortcuts import redirect
 from django.urls import reverse
+from rango.forms import UserForm, UserProfileForm
+from django.contrib.auth import authenticate, login, logout
+
+
 def index(request):
     # Query the database for a list of ALL categories currently stored.
     # Order the categories by the number of likes in descending order.
@@ -34,7 +38,8 @@ def about(request):
     #we make use of the shortcut function to make our lives easier
     #note that the first parameter is the template we wish to use
     return render(request,'rango/about.html',context=context_dict)
-  
+
+@login_required  
 def add_category(request):
     form = CategoryForm()
     
@@ -59,7 +64,7 @@ def add_category(request):
     # Render the form with error messages (if any).
     return render(request, 'rango/add_category.html', {'form': form})
  
- 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -122,4 +127,136 @@ def show_category(request, category_name_slug):
     # Go render the response and return it to the client.
     return render(request, 'rango/category.html', context=context_dict)
     
-    
+ 
+def register(request):
+    # A boolean value for telling the template
+    # whether the registration was successful.
+    # Set to False initially. Code changes value to
+    # True when registration succeeds.
+    registered = False
+
+    # If it's a HTTP POST, we're interested in processing form data.
+    if request.method == 'POST':
+        # Attempt to grab information from the raw form information.
+        # Note that we make use of both UserForm and UserProfileForm.
+        user_form = UserForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+
+        # If the two forms are valid...
+        if user_form.is_valid() and profile_form.is_valid():
+            # Save the user's form data to the database.
+            user = user_form.save()
+
+            # Now we hash the password with the set_password method.
+            # Once hashed, we can update the user object.
+            user.set_password(user.password)
+            user.save()
+
+            # Now sort out the UserProfile instance.
+            # Since we need to set the user attribute ourselves,
+            # we set commit=False. This delays saving the model
+
+            # until we're ready to avoid integrity problems.
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            # Did the user provide a profile picture?
+            # If so, we need to get it from the input form and
+            #put it in the UserProfile model.
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+
+            # Now we save the UserProfile model instance.
+            profile.save()
+
+            # Update our variable to indicate that the template
+            # registration was successful.
+            registered = True
+        else:
+            # Invalid form or forms - mistakes or something else?
+            # Print problems to the terminal.
+            print(user_form.errors, profile_form.errors)
+    else:
+        # Not a HTTP POST, so we render our form using two ModelForm instances.
+        # These forms will be blank, ready for user input.
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    # Render the template depending on the context.
+    return render(request, 'rango/register.html',
+                context = {'user_form': user_form,
+                            'profile_form': profile_form,
+                            'registered': registered})
+                            
+def user_login(request):
+	# If the request is a HTTP POST, try to pull out the relevant information.
+	if request.method == 'POST':
+		# Gather the username and password provided by the user.
+		# This information is obtained from the login form.
+		# We use request.POST.get('<variable>') as opposed
+		# to request.POST['<variable>'], because the
+		# request.POST.get('<variable>') returns None if the
+		# value does not exist, while request.POST['<variable>']
+		# will raise a KeyError exception.
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+		# Use Django's machinery to attempt to see if the username/password
+		# combination is valid - a User object is returned if it is.
+		user = authenticate(username=username, password=password)
+		# If we have a User object, the details are correct.
+		# If None (Python's way of representing the absence of a value), no user
+		# with matching credentials was found.
+		if user:
+			# Is the account active? It could have been disabled.
+			if user.is_active:
+				# If the account is valid and active, we can log the user in.
+				# We'll send the user back to the homepage.
+				login(request, user)
+				return redirect(reverse('rango:index'))
+			else:
+				# An inactive account was used - no logging in!
+				return HttpResponse("Your Rango account is disabled.")
+		else:
+			# Bad login details were provided. So we can't log the user in.
+			print(f"Invalid login details: {username}, {password}")
+			return HttpResponse("Invalid login details supplied.")
+	# The request is not a HTTP POST, so display the login form.
+	# This scenario would most likely be a HTTP GET.
+	else:
+		# No context variables to pass to the template system, hence the
+		# blank dictionary object...
+		return render(request, 'rango/login.html')
+		
+@login_required
+def user_logout(request):
+	# Since we know the user is logged in, we can now just log them out.
+	logout(request)
+	# Take the user back to the homepage.
+	return redirect(reverse('rango:index'))
+		
+@login_required
+def restricted(request):
+	return HttpResponse("Since you're logged in, you can see this text!")
+
+def get_server_side_cookie(request, cookie, default_val=None):
+	val = request.session.get(cookie)
+	if not val:
+		val = default_val
+	return val	
+	
+def visitor_cookie_handler(request):
+	visits = int(get_server_side_cookie(request, 'visits', '1'))
+	last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+	last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+	
+	# If it's been more than a day since the last visit...
+	if (datetime.now() - last_visit_time).days > 0:
+		visits = visits + 1
+		# Update the last visit cookie now that we have updated the count
+		request.session['last_visit'] = str(datetime.now())
+	else:
+		# Set the last visit cookie
+		request.session['last_visit'] = last_visit_cookie
+	
+	# Update/set the visits cookie
+	request.session['visits'] = visits
